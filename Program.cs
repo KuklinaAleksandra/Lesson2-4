4fae03c23a2c5ed9ec983f9defe584ea81fc88be
Aleksandra <kuklina.aleksandra@rambler.ru>
2021-04-02 20:47:25 +0600

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson2_4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string shop = "Брендированный электронный \n чек для отправки  ";
+            string thing = "Футболка  ";
+            int price = 500;
+            int quantity = 3;
+            int sum = price * quantity;
+            double nds20 = sum * 0.2;
+            Console.WriteLine( shop + "  \n" + thing + price + " x "+  quantity + " = " + sum + "  \n" + "НДС 20%              " + nds20);
+            Console.WriteLine("  ");
+            Console.ReadKey();
+        }
+    }
+}
